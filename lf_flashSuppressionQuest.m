ad51643f707e841671d1a154b04dc8c35786ff70
Lucas Feldman <lucfeldmann23@googlemail.com>
2017-05-29 13:30:34 +0200

    conversion to function
    converted to function to have everything in one script

--- lf_flashSuppressionQuest.m
+++ lf_flashSuppressionQuest.m
@@ -1,3 +1,4 @@
+function [] = lf_flashSuppressionQuest()
 %%  Info section
 % ----------------------------------------------------------------------- %
 %   Author: Lucas Feldmann
@@ -11,7 +12,7 @@ sca; close all; clear all; clc;
 global window;
 %% Set experiment parameters
 % Side of the stimulus to appear, 0 for left, 1 for right
-side = 0;
+side = 1;
 % Path to the frame image
 frameImagePath = 'X:\Mitarbeiter\Chris\MatLab\CFS_Frame.jpg';
 % Path to the image of the first stimulus (e.g. Mondrian)
@@ -32,17 +33,13 @@ durationFix = 1;
 % Duration of the first stimulus in seconds
 durationFirstStimulus = 2;
 % Duration of the seconds stimulus in seconds
-durationSecondStimulus = 0.3;
-% Number of different interstimulus intervals
-numberOfIsi = 7;
-% Number of trials per interstimulus interval
-numberOfTrialsPerIsi = 9;
+durationSecondStimulus = 0.5;
 % Total number of trials
-numberOfTrials = numberOfIsi * numberOfTrialsPerIsi;
+numberOfTrials = 50;
 % Number of pre trials
-numberOfPreTrials = 3;
+numberOfPreTrials = 10;
 % Guess initial threshhold (numeric scale, e.g. 0.05)
-initialGuess = 0.05;
+initialGuess = 0.20;
 % Guess initial sd (log scale, e.g. 10)
 initialSd = 10;
 % Key for response 'yes'
@@ -50,28 +47,7 @@ yes = 'y';
 % Key for response 'no'
 no = 'n';
 % Key for experiment termination
-escape = 'ESCAPE'; 
-%% Generate trial matrix for main procedure
-isi_matrix = zeros(numberOfTrialsPerIsi,numberOfIsi);
-randi([0, 2000], 1,numberOfTrialsPerIsi);
-isi_matrix(:,1) = (randi([0, 2000], 1,9))/1000;
-for i = 2:size(isi_matrix,2)
-    shuffled_block = Shuffle(isi_matrix(:,1));
-    isi_matrix(:,i) = shuffled_block;
-end
-% Details for condition matrix
-logicals = zeros(numberOfTrials,1);
-logicals(1:(round(numberOfTrials/2))) = 1;
-logicals_shuffled = Shuffle(logicals);
-trials = 1:numberOfTrialsPerIsi;
-alltrials = repmat(trials,1,numberOfIsi)';
-A = (1:7);
-repeated = sort(repmat(A, 1,numberOfTrialsPerIsi));
-% Fill final condition matrix
-trialmatrix(:,1) = alltrials;
-trialmatrix(:,2) = repeated;
-trialmatrix(:,3) = isi_matrix(:); % Fügt die Spalten hintereinander
-trialmatrix(:,4) = logicals_shuffled;
+escape = 'ESCAPE';
 %% Set pre QUEST parameters
 pThresholdpre=0.82; betapre=3.5; deltapre=0.01; gammapre=0.5; grainpre=0.1; rangepre=8;
 qpre=QuestCreate(log10(initialGuess),initialSd,pThresholdpre,betapre,deltapre,gammapre,grainpre,rangepre);
@@ -163,11 +139,11 @@ if response == 99
 else
 % Get preliminary estimate of threshold.
 tpreLog=QuestMean(qpre);
-tpre=10^tpreLog;
+tpre=max(0,min(1,10^tpreLog));
 sdpre=QuestSd(qpre);
 fprintf('Preliminary threshhold estimate (mean+-sd) is %.5f +- %.5f\n',tpre,sdpre);
 %% Set main QUEST parameters based on preliminary test
-tGuess=tpre;
+tGuess=log10(tpre);
 tGuessSd=initialSd;
 pThreshold=0.82; beta=3.5; delta=0.01; gamma=0.5; grain=0.01; range=10;
 q=QuestCreate(tGuess,tGuessSd,pThreshold,beta,delta,gamma,grain,range);
@@ -180,11 +156,7 @@ for i = 1:numberOfTrials
     intensity = 10^intensityLog;
     %% Experiment procure 
     % Limit intensity to 0-1 range    
-    intensity=max(0,min(1,intensity));
-    % Set ISI duration based on trial matrix
-    durationFirstStimulus = trialmatrix(i,3);
-    % Set side based on trial matrix
-    side = trialmatrix(i,4);
+    intensity=max(0,min(1,intensity));   
     % Get response using the new intensity    
     response = lf_showFlashSuppressionSequence(intensity, black, durationFix, durationFirstStimulus, durationSecondStimulus, frameTex, firstStimTex, secondStimTex, thirdStim, LeftPosition, RightPosition, imageRectInLeftFrame, imageRectInRightFrame, escapeKey, yesKey, noKey, side);
     % End if escape has been pressed
@@ -204,3 +176,96 @@ sd=QuestSd(q);
 fprintf('Final threshhold estimate (mean+-sd) is %.5f +- %.5f\n',t,sd);
 end
 sca;
+end
+
+function [response] = lf_showFlashSuppressionSequence(intensity, black, durationFix, durationFirstStimulus, durationSecondStimulus, frameTex, firstStimTex, secondStimTex, thirdStim, LeftPosition, RightPosition, imageRectInLeftFrame, imageRectInRightFrame, escapeKey, yesKey, noKey, side)
+%%  Info section
+% ----------------------------------------------------------------------- %
+%   Author: Lucas Feldmann
+%   Version: 0.1
+%   Date: 20170524
+%   About: Displays one trial procedure of the flash supression experiment
+% ----------------------------------------------------------------------- %
+%   Function name: 
+%   Input parameters:
+%       intensity: The contrast of the third stimulus (weight)
+%       black: Black background color
+%       durationFix: Duration of the fixation phase
+%       durationFirstStimulus: Duration of first stimulus presentation
+%       durationSecondStimulus: Duration of second stimulus presentation
+%       frameTex: Texture of the frame
+%       firstStimTex: Texture of the first stimlus
+%       secondStimTex: Texture of the second stimulus
+%       thirdStim: Image map of the third stimulus (no texture!)
+%       LeftPosition: Left frame position
+%       RightPosition: Right frame position
+%       imageRectInLeftFrame: Left image rectangle
+%       imageRectInRightFrame: Right image rectangle
+%       escapeKey: Keycode for the key to terminate the experiment
+%       yesKey: Keycode for response 'yes' 
+%       noKey: Keycode for response 'no'
+% ----------------------------------------------------------------------- %
+global window        
+%% Show just the empty frames 
+Screen('DrawTexture', window, frameTex, [], LeftPosition, 0);
+Screen('DrawTexture', window, frameTex, [], RightPosition, 0);
+Screen('Flip', window);
+WaitSecs (durationFix); 
+%% Update textures with new contrast during wait
+thirdStimWithContrast = (thirdStim-thirdStim(1)).*intensity + thirdStim(1);
+thirdStimTex = Screen('MakeTexture', window, thirdStimWithContrast);
+%% Show stimuli depending on side
+if side == 0 % left side
+    %% Show first stimulus 
+    Screen('DrawTexture', window, frameTex, [], LeftPosition, 0);
+    Screen('DrawTexture', window, frameTex, [], RightPosition, 0);
+    Screen('DrawTexture', window, firstStimTex, [], imageRectInLeftFrame, 0);
+    Screen('Flip', window);
+    WaitSecs (durationFirstStimulus);    
+    %% Show second and third stimulus
+    Screen('DrawTexture', window, frameTex, [], LeftPosition, 0);
+    Screen('DrawTexture', window, frameTex, [], RightPosition, 0);
+    Screen('DrawTexture', window, secondStimTex, [], imageRectInRightFrame, 0);
+    Screen('DrawTexture', window, thirdStimTex, [], imageRectInLeftFrame, 0);
+    Screen('Flip', window);
+    WaitSecs (durationSecondStimulus);   
+    Screen(window, 'FillRect', black);
+    Screen('Flip', window);
+elseif side == 1 % right side
+    %% Show first stimulus 
+    Screen('DrawTexture', window, frameTex, [], LeftPosition, 0);
+    Screen('DrawTexture', window, frameTex, [], RightPosition, 0);
+    Screen('DrawTexture', window, firstStimTex, [], imageRectInRightFrame, 0);
+    Screen('Flip', window);
+    WaitSecs (durationFirstStimulus);    
+    %% Show second and third stimulus
+    Screen('DrawTexture', window, frameTex, [], LeftPosition, 0);
+    Screen('DrawTexture', window, frameTex, [], RightPosition, 0);
+    Screen('DrawTexture', window, secondStimTex, [], imageRectInLeftFrame, 0);
+    Screen('DrawTexture', window, thirdStimTex, [], imageRectInRightFrame, 0);
+    Screen('Flip', window);
+    WaitSecs (durationSecondStimulus);   
+    Screen(window, 'FillRect', black);
+    Screen('Flip', window);
+else
+    disp('***Invalid side!***');
+end   
+%% Get response 
+% show message to vps to report whether they have seen the face or not
+% loop through keypresses until a correct key is pressed
+while 1
+    [~ , keyCode, ~] = KbWait([], 2);   
+    if keyCode(yesKey)        
+        response = 1;  
+        break;
+    elseif keyCode(noKey)
+        response = 0;  
+        break;
+    elseif keyCode(escapeKey)            
+        response = 99;              
+        break;
+    else           
+        disp('***Invalid key press.***');            
+    end   
+end
+end
